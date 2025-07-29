ccdf12664a6d6df391a5cc12e31e6d485821a0a2
Khanhan010903 <quangan010903@gmail.com>
2025-07-29 11:13:33 +0700

    completed section 3

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0449659e20aeeb601f59e5ab4c897b930cea3ba0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d64a5ec44986dd534f768d96ba92a2ee8b9c267b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
