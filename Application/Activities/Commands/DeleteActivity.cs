a35ddc9812d8e407d83962dad2565bbc611df089
Khanhan010903 <quangan010903@gmail.com>
2025-07-29 14:41:34 +0700

    End of section 4

---
+++ Application/Activities/Commands/DeleteActivity.cs
@@ -0,0 +1,23 @@
+using System;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities.Commands;
+
+public class DeleteActivity
+{
+    public class Commands : IRequest
+    {
+        public required string Id { get; set; }
+    }
+    public class Handler(AppDbContext context) : IRequestHandler<Commands>
+    {
+        public async Task Handle(Commands request, CancellationToken cancellationToken)
+        {
+            var activity = await context.Activities.FindAsync([request.Id], cancellationToken)
+                            ?? throw new Exception("Cannot find activity");
+            context.Remove(activity);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccdf12664a6d6df391a5cc12e31e6d485821a0a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6193b0f271a46c1876b35a5ed1009e4c164cd14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
